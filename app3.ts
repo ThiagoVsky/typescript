c2f00b660d93797b2938f69dd734be04ee16d8a4
Thiago Moraes <thiago.moraes582@gmail.com>
2018-04-23 23:22:18 -0300

    app3 incluido

---
+++ app3.ts
@@ -0,0 +1,10 @@
+import Carro from "./Carro";
+import Concessionaria from "./Concessionaria";
+import Pessoa from "./Pessoa";
+import Moto from "./moto";
+
+let carroA = new Carro("HB20", 4)
+let motoA = new Moto("Ninja 300")
+
+console.log(carroA)
+console.log(motoA)
