df0eca6e9de3afa3c10a8bf67a5187d42630e1fa
Thiago Moraes <thiago.moraes582@gmail.com>
2018-04-23 22:33:23 -0300

    atualização 02

---
+++ JavaScript/veiculo.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Veiculo = /** @class */ (function () {
+    function Veiculo() {
+        this.velocidade = 0;
+    }
+    return Veiculo;
+}());
+exports.default = Veiculo;
+acelerar();
+void {
+    this: .velocidade += 10
+};
+parar();
+void {
+    this: .velocidade = 0
+};
+velocidadeAtual();
+number;
+{
+    return this.velocidade;
+}
